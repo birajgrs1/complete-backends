4bf5e8f7fc0ad92a4cb43c7d02663504f178bc9a
birajgrs <birajghorasaine11@gmail.com>
2025-06-18 13:16:47 +0545

    basic setup for airbnb

---
+++ ii. expressjs/airbnb-proj/1.airbnb-basic/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h2>Welcome to AirBnB!</h2>
+    <a href="/add-home">Go to Add Home</a>
+  `);
+});
+
+
+app.get("/add-home", (req, res) => {
+  res.send(`
+    <h2>Add Home</h2>
+    <form method="POST" action="/add-home">
+      <input type="text" name="houseName" placeholder="houseName"/>
+      <input type="text" name="address" placeholder="address"/>
+      <input type="submit" value="Submit"/>
+    </form>
+  `);
+});
+
+app.post("/add-home", (req, res) => {
+  console.log(req.body);
+  res.send("Home added successfully!");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
